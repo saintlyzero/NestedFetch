b7db74ab76a8cf751466b95b5494306df9371318
= <=>
2019-10-27 01:45:28 +0530

    fixed setup

--- setup.py
+++ setup.py
@@ -32,6 +32,9 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'License :: OSI Approved :: MIT License',
-    'Programming Language ::  Python :: 3 :: Only'
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
   ]
 )
